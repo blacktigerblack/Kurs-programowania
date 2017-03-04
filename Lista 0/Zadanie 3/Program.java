84b3d9266d13a4e311832dfcef47401e59bfa037
blacktigerblack <djaworek97@gmail.com>
2017-03-04 11:09:10 +0100

    Update Program.java

--- Lista 0/Zadanie 3/Program.java
+++ Lista 0/Zadanie 3/Program.java
@@ -4,15 +4,23 @@ public class Program {
         for(int i = 0; i < args.length; i++) {
             try {
                 n = Integer.parseInt(args[i]);
+                if(n <= 0)
+                    throw new NumberFormatException();
                 System.out.println(n + ": " + div(n));
             }
             catch(NumberFormatException ex) {
-                System.out.println(args[i] + " nie jest liczba calkowita");
+                System.out.println(args[i] + " nie jest liczba naturalna");
             }
         }
     }
 
     public static int div(int n) {
-        return n;
+        int d = 1;
+        for(int i = 1; i <= n/2; i++) {
+            if(n % i == 0)
+                d = i;
+        }
+        return d;
     }
 }
+
